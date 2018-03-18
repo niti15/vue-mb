988f31cdad162d0c955ca9923e1ff2b0426ae1d8
Nithya <nithya@cogzidel.com>
2018-03-18 15:13:20 +0530

    check

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var app = express();
 var port = process.env.PORT || 5000
 app.listen(port);
 console.log('starting project at ' + port);
-app.engine('pug', require('pug').__express);
+// app.engine('pug', require('pug').__express);
 app.set('views', '/');
-app.set('view engine', 'pug');
+app.set('view engine', '');
 //Look for statics first
 app.use(serveStatic(path.join(__dirname, '/')));
 //Return the index for any other GET request
